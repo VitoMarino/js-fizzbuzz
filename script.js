faf36fde3621b82352189ba17546d71fc56ec4e7
VitoMarino <marino.vito2001@gmail.com>
2024-05-10 15:43:26 +0200

    Console multipli di 3 e 5

--- script.js
+++ script.js
@@ -4,6 +4,19 @@
 // per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+// i = 1 è il contatore. quello dopo (i<=100) è la condizione del contatore, che può essere vera o falsa. 
+// se è vera la condizione esegue il ciclo.
+// i++ è "di quanto aumenta il contatore" e il ciclo si interrompe quando la condizione dettata termina.
+let Fizz = '';
+let Buzz = '';
+let FizzBuzz = '';
+
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    if (i % 3 == 0) {
+    console.log(i, );
+    } else if (i % 5 == 0) {
+        console.log(i);
+    }
+    
 }
+
