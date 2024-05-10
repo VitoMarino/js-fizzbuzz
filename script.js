7c47f1c84e3dd515fbaa227fb7ecc81b92f261aa
VitoMarino <marino.vito2001@gmail.com>
2024-05-10 16:43:16 +0200

    else finale

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ for (let i = 1; i <= 100; i++) {
         console.log(Buzz);
     } else if (i % 3 !== 0 && i % 5 !== 0) {
         console.log(i);
-    } else (i % 3 === 0 && i % 5 === 0)
+    } else { (i % 3 === 0 && i % 5 === 0)
         console.log(FizzBuzz);
+    }
 }
